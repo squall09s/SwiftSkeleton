6d88bbe7245c3e7b34fc80716b248427988594ae
Nicolas Laurent <squall.leonhart@me.com>
2024-10-15 13:05:15 +0200

    Clean Code

--- Sources/SwiftSkeleton/main.swift
+++ Sources/SwiftSkeleton/main.swift
@@ -15,55 +15,106 @@ guard CommandLine.argc > 1 else {
 let jsonFilePath = CommandLine.arguments[1] // The first argument is the path of the JSON file
 let jsonFileURL = URL(fileURLWithPath: jsonFilePath)
 
-// Retrieve parent directory of JSON file
-let parentDirectory = jsonFileURL.deletingLastPathComponent()
+let generator = ProjectGenerator(jsonFileURL : jsonFileURL)
+generator.generateProject()
 
-let fileManager = FileManager.default
-
-@MainActor func generateProject(){
+class ProjectGenerator {
     
-    do {
-        
-        let jsonData = try Data(contentsOf: jsonFileURL)
-        
-        let decoder = JSONDecoder()
-        let config = try decoder.decode(AppConfiguration.self, from: jsonData)
-        
-        for module in config.modules {
-            
-            let coordinator = config.coordinators?.flows.first(where: { _coordinator in
-                return  _coordinator.root == module.id || (_coordinator.childs ?? [] ).contains([module.id])
-            })
-            
-            createModuleFiles(module : module, appExtensions: AppExtensions(coordinator: coordinator), projectName : config.project_name)
-        }
-        
-        createAppCoordinatorFiles(appConfig: config)
-        createCoordinatorChildsFiles(appConfig: config)
-        
-        
-    } catch {
-        print("Erreur lors de la lecture ou du décodage du fichier JSON: \(error)")
+    let fileManager = FileManager.default
+    let jsonFileURL : URL
+    
+    
+    // Retrieve parent directory of JSON file
+    func parentDirectory() -> URL {
+        return jsonFileURL.deletingLastPathComponent()
     }
     
-}
+    init(jsonFileURL : URL){
+        self.jsonFileURL = jsonFileURL
+    }
+    
+    func generateProject()  {
+        
+        do {
+            
+            let jsonData = try Data(contentsOf: jsonFileURL)
+            
+            let decoder = JSONDecoder()
+            let config = try decoder.decode(AppConfiguration.self, from: jsonData)
+            
+            for module in config.modules {
+                
+                let coordinator = config.coordinators?.flows.first(where: { _coordinator in
+                    return  _coordinator.root == module.id || (_coordinator.childs ?? [] ).contains([module.id])
+                })
+                
+                createModuleFiles(module : module, appExtensions: AppExtensions(coordinator: coordinator), projectName : config.project_name)
+            }
+            
+            createAppCoordinatorFiles(appConfig: config)
+            createCoordinatorChildsFiles(appConfig: config)
+            
+        } catch {
+            print("Erreur lors de la lecture ou du décodage du fichier JSON: \(error)")
+        }
+        
+    }
 
-@MainActor func createModuleFiles(module : ModuleConfiguration, appExtensions: AppExtensions, projectName: String){
-    
-    var templates = [ModuleTemplate]()
-    
-    templates.append(ViewController_template(module: module, projectName: projectName, extensions: appExtensions))
-    templates.append(Presenter_template(module: module, projectName: projectName, extensions: appExtensions))
-    templates.append(Contracts_Template(module: module, projectName: projectName, extensions: appExtensions))
-    templates.append(Storyboard_Template(module: module, projectName: projectName, extensions: appExtensions))
-    templates.append(Builder_Template(module: module, projectName: projectName, extensions: appExtensions))
-    
-    for template in templates {
+    func createModuleFiles(module : ModuleConfiguration, appExtensions: AppExtensions, projectName: String){
         
-        try? fileManager.createDirectory(at: parentDirectory.appending(path: "/" + projectName + "/Sources/" + module.identifier()), withIntermediateDirectories: true)
+        var templates = [ModuleTemplate]()
         
-        var filePath = parentDirectory.appending(path: "/" + projectName + "/Sources/\(module.identifier())/")
-        filePath = filePath.appending(path: template.fileName() + template.type.fileExtension())
+        templates.append(ViewController_template(module: module, projectName: projectName, extensions: appExtensions))
+        templates.append(Presenter_template(module: module, projectName: projectName, extensions: appExtensions))
+        templates.append(Contracts_Template(module: module, projectName: projectName, extensions: appExtensions))
+        templates.append(Storyboard_Template(module: module, projectName: projectName, extensions: appExtensions))
+        templates.append(Builder_Template(module: module, projectName: projectName, extensions: appExtensions))
+        
+        for template in templates {
+            
+            try? fileManager.createDirectory(at: parentDirectory().appending(path: "/" + projectName + "/Sources/" + module.identifier()), withIntermediateDirectories: true)
+            
+            var filePath = parentDirectory().appending(path: "/" + projectName + "/Sources/\(module.identifier())/")
+            filePath = filePath.appending(path: template.fileName() + template.type.fileExtension())
+            
+            if let data = template.export().data(using: .utf8) {
+                
+                do {
+                    
+                    //if fileManager.fileExists(atPath: filePath.path()) {
+                    //    print("The file already exists, it will be replaced.")
+                    //}
+                    
+                    // Write data to file
+                    try data.write(to: filePath)
+                    //print("File successfully created at \(filePath)")
+                } catch {
+                    print("Error creating file : \(error)")
+                }
+            } else {
+                print("Unable to convert content to UTF-8")
+            }
+            
+        }
+        
+        print("Module {\(module.identifier())} created successfully (\(templates.count) files).")
+        
+    }
+
+
+    /// EXTENSION COORDINATOR
+    ///
+    func createAppCoordinatorFiles(appConfig : AppConfiguration){
+        
+        guard let coordinator = appConfig.coordinators else {
+            return
+        }
+        
+        try? fileManager.createDirectory(at: parentDirectory().appending(path: "/" + appConfig.project_name + "/Coordinators/"), withIntermediateDirectories: true)
+        
+        let filePath = parentDirectory().appending(path:  "/" + appConfig.project_name + "/Coordinators/AppCoordinator.swift" )
+        
+        let template = AppCoordinator_Template(coordinatorConfiguration: coordinator, projectName: appConfig.project_name)
         
         if let data = template.export().data(using: .utf8) {
             
@@ -75,7 +126,9 @@ let fileManager = FileManager.default
                 
                 // Write data to file
                 try data.write(to: filePath)
-                //print("File successfully created at \(filePath)")
+                
+                print("AppCordinator successfully created with \(coordinator.initals?.count ?? 0) root coordinators.")
+                
             } catch {
                 print("Error creating file : \(error)")
             }
@@ -83,141 +136,101 @@ let fileManager = FileManager.default
             print("Unable to convert content to UTF-8")
         }
         
-    }
-    
-    print("Module {\(module.identifier())} created successfully (\(templates.count) files).")
-    
-}
-
-
-/// EXTENSION COORDINATOR
-///
-@MainActor func createAppCoordinatorFiles(appConfig : AppConfiguration){
-    
-    guard let coordinator = appConfig.coordinators else {
-        return
-    }
-    
-    try? fileManager.createDirectory(at: parentDirectory.appending(path: "/" + appConfig.project_name + "/Coordinators/"), withIntermediateDirectories: true)
-    
-    let filePath = parentDirectory.appending(path:  "/" + appConfig.project_name + "/Coordinators/AppCoordinator.swift" )
-    
-    let template = AppCoordinator_Template(coordinatorConfiguration: coordinator, projectName: appConfig.project_name)
-    
-    if let data = template.export().data(using: .utf8) {
         
-        do {
-            
-            //if fileManager.fileExists(atPath: filePath.path()) {
-            //    print("The file already exists, it will be replaced.")
-            //}
-            
-            // Write data to file
-            try data.write(to: filePath)
-            
-            print("AppCordinator successfully created with \(coordinator.initals?.count ?? 0) root coordinators.")
-            
-        } catch {
-            print("Error creating file : \(error)")
-        }
-    } else {
-        print("Unable to convert content to UTF-8")
     }
-    
-    
-}
 
 
-
-@MainActor func createCoordinatorChildsFiles(appConfig : AppConfiguration){
-    
-    
-    for coordinator in appConfig.coordinators?.flows ?? [] {
+    func createCoordinatorChildsFiles(appConfig : AppConfiguration){
         
-        switch coordinator.type {
+        
+        for coordinator in appConfig.coordinators?.flows ?? [] {
             
-        case .navigationController:
-            
-            let moduleRoot = appConfig.modules.first(where: { $0.id == coordinator.root ?? "" })!
-            let modulesChilds = coordinator.childs?.compactMap({ _childID in
-                return appConfig.modules.first(where: { $0.id == _childID })
-            }) ?? []
-            
-            let template = NavigationControllerCoordinator_Template(coordinator: coordinator, projectName: appConfig.project_name, root: moduleRoot, childs: modulesChilds)
-            
-            try? fileManager.createDirectory(at: parentDirectory.appending(path: "/" + appConfig.project_name + "/Coordinators/"), withIntermediateDirectories: true)
-            
-            var filePath = parentDirectory.appending(path: "/" + appConfig.project_name + "/Coordinators/")
-            filePath = filePath.appending(path: template.fileName() + TypeFile.naviationCoordinator.fileExtension())
-            
-            // Convertir le contenu en données UTF-8
-            if let data = template.export().data(using: .utf8) {
-                // Écrire dans le fichier
-                do {
-                    
-                    //if fileManager.fileExists(atPath: filePath.path()) {
-                    //print("The file already exists, it will be replaced.")
-                    //}
-                    
-                    // Write data to file
-                    try data.write(to: filePath)
-                    
-                    print("\(template.fileName()) successfully created (it's a navigation coordinator).")
-                    
-                } catch {
-                    print("Error creating file : \(error)")
-                }
-            } else {
-                print("Unable to convert content to UTF-8")
-            }
-            
-        case .tabbarController:
-            
-            var tabsConfigurations : [TabConfiguration] = [TabConfiguration]()
-            
-            for childIdentifier in coordinator.childs ?? [] {
+            switch coordinator.type {
                 
-                if childIdentifier.hasPrefix("flow:") {
-                    
-                    if let _coordinator = appConfig.coordinators?.flows.first(where: { $0.identifier() == String(childIdentifier.split(separator: ":").last!) }){
-                        tabsConfigurations.append( .coordinator(coordinator: _coordinator) )
+            case .navigationController:
+                
+                let moduleRoot = appConfig.modules.first(where: { $0.id == coordinator.root ?? "" })!
+                let modulesChilds = coordinator.childs?.compactMap({ _childID in
+                    return appConfig.modules.first(where: { $0.id == _childID })
+                }) ?? []
+                
+                let template = NavigationControllerCoordinator_Template(coordinator: coordinator, projectName: appConfig.project_name, root: moduleRoot, childs: modulesChilds)
+                
+                try? fileManager.createDirectory(at: parentDirectory().appending(path: "/" + appConfig.project_name + "/Coordinators/"), withIntermediateDirectories: true)
+                
+                var filePath = parentDirectory().appending(path: "/" + appConfig.project_name + "/Coordinators/")
+                filePath = filePath.appending(path: template.fileName() + TypeFile.naviationCoordinator.fileExtension())
+                
+                // Convertir le contenu en données UTF-8
+                if let data = template.export().data(using: .utf8) {
+                    // Écrire dans le fichier
+                    do {
+                        
+                        //if fileManager.fileExists(atPath: filePath.path()) {
+                        //print("The file already exists, it will be replaced.")
+                        //}
+                        
+                        // Write data to file
+                        try data.write(to: filePath)
+                        
+                        print("\(template.fileName()) successfully created (it's a navigation coordinator).")
+                        
+                    } catch {
+                        print("Error creating file : \(error)")
                     }
+                } else {
+                    print("Unable to convert content to UTF-8")
+                }
+                
+            case .tabbarController:
+                
+                var tabsConfigurations : [TabConfiguration] = [TabConfiguration]()
+                
+                for childIdentifier in coordinator.childs ?? [] {
                     
-                }else{
-                    
-                    if let module = appConfig.modules.first(where: { $0.id == childIdentifier }){
-                        tabsConfigurations.append( .module(module : module) )
+                    if childIdentifier.hasPrefix("flow:") {
+                        
+                        if let _coordinator = appConfig.coordinators?.flows.first(where: { $0.identifier() == String(childIdentifier.split(separator: ":").last!) }){
+                            tabsConfigurations.append( .coordinator(coordinator: _coordinator) )
+                        }
+                        
+                    }else{
+                        
+                        if let module = appConfig.modules.first(where: { $0.id == childIdentifier }){
+                            tabsConfigurations.append( .module(module : module) )
+                        }
+                        
                     }
                     
                 }
                 
-            }
-            
-            let template = TabbarControllerCoordinator_Template(coordinator: coordinator, projectName: appConfig.project_name, tabs: tabsConfigurations)
-            
-            try? fileManager.createDirectory(at: parentDirectory.appending(path: "/" + appConfig.project_name + "/Coordinators/"), withIntermediateDirectories: true)
-            
-            var filePath = parentDirectory.appending(path: "/" + appConfig.project_name + "/Coordinators/")
-            filePath = filePath.appending(path: template.fileName() + TypeFile.tabBarCoordinator.fileExtension())
-            
-            if let data = template.export().data(using: .utf8) {
+                let template = TabbarControllerCoordinator_Template(coordinator: coordinator, projectName: appConfig.project_name, tabs: tabsConfigurations)
                 
-                do {
+                try? fileManager.createDirectory(at: parentDirectory().appending(path: "/" + appConfig.project_name + "/Coordinators/"), withIntermediateDirectories: true)
+                
+                var filePath = parentDirectory().appending(path: "/" + appConfig.project_name + "/Coordinators/")
+                filePath = filePath.appending(path: template.fileName() + TypeFile.tabBarCoordinator.fileExtension())
+                
+                if let data = template.export().data(using: .utf8) {
                     
-                    //if fileManager.fileExists(atPath: filePath.path()) {
-                    //print("The file already exists, it will be replaced.")
-                    //}
-                    
-                    // Write data to file
-                    try data.write(to: filePath)
-                    
-                    print("\(template.fileName()) successfully created (it's a tabbar coordinator).")
-                } catch {
-                    print("Error creating file : \(error)")
+                    do {
+                        
+                        //if fileManager.fileExists(atPath: filePath.path()) {
+                        //print("The file already exists, it will be replaced.")
+                        //}
+                        
+                        // Write data to file
+                        try data.write(to: filePath)
+                        
+                        print("\(template.fileName()) successfully created (it's a tabbar coordinator).")
+                    } catch {
+                        print("Error creating file : \(error)")
+                    }
+                } else {
+                    print("Unable to convert content to UTF-8")
                 }
-            } else {
-                print("Unable to convert content to UTF-8")
             }
         }
     }
+
 }
